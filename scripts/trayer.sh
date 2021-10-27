55ddf6962ee82e89b7eb14679baa416fa9a5c14a
plokid <42007061@xs.ustb.edu.cn>
2021-10-27 21:02:46 +0800

    删除awesome patch,修改trayer长度

--- scripts/trayer.sh
+++ scripts/trayer.sh
@@ -4,5 +4,5 @@ killall trayer
 if [ $? != 0 ];then
     nm-applet &
     blueman-applet &
-    trayer --edge top --align right --widthtype percent --width 17 --transparent 30 --heighttype pixel --height 30 --monitor primary &
+    trayer --edge top --align right --widthtype percent --width 13 --transparent 30 --heighttype pixel --height 30 --monitor primary &
 fi
