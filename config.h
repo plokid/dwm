9a1806f24d8d372564c87db30ce18f8fa9569801
plokid <42007061@xs.ustb.edu.cn>
2021-11-12 22:50:08 +0800

    修改rofi运行run为drun

--- config.h
+++ config.h
@@ -90,7 +90,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "rofi", "-show", "run", NULL };
+static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *flameshotcmd[]  = { "flameshot", "gui", NULL };
 static const char *trayercmd[]  = { "/bin/sh", "-c", "$HOME/.dwm/scripts/trayer.sh", NULL };
